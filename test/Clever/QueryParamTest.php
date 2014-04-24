752a038d82bac5777342476e7fb3dba6539bf426
Rafael Garcia <rafael.garcia@clever.com>
2014-04-24 13:48:39 -0700

    update for new demo data

--- test/Clever/QueryParamTest.php
+++ test/Clever/QueryParamTest.php
@@ -7,14 +7,14 @@ class CleverQueryParamTest extends UnitTestCase
     authorizeFromEnv();
 
     $validQueries = array(
-      array('where' => json_encode(array('name' => 'Clever Memorial High'))),
-      array('where' => '{"name": {"$regex": "Memorial"}}'),
-      array('where' => array('name' => 'Clever Memorial High'))
+      array('where' => json_encode(array('name' => 'Clever High School'))),
+      array('where' => '{"name": {"$regex": "High"}}'),
+      array('where' => array('name' => 'Clever High School'))
     );
     foreach ($validQueries as $query) {
       $schools = CleverSchool::all($query);
       $this->assertEqual(count($schools), 1);
-      $this->assertEqual($schools[0]->name, 'Clever Memorial High');
+      $this->assertEqual($schools[0]->name, 'Clever High School');
     }
   }
 
