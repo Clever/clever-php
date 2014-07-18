6387c8d9c88fd57db26ea71fe1ce707bae53e244
henderjon <jhenderson@capstonedigital.com>
2014-07-18 11:13:54 -0500

    replaces error_log() with $logger->notice()

--- lib/Clever/Object.php
+++ lib/Clever/Object.php
@@ -10,6 +10,7 @@ class CleverObject implements ArrayAccess
 
   protected $_auth;
   protected $_values;
+  protected $logger;
 
   public function __construct($id=null, $auth=null)
   {
@@ -17,6 +18,12 @@ class CleverObject implements ArrayAccess
     $this->_values = array();
     if ($id)
       $this->id = $id;
+
+
+    $this->logger = \Clever::$logger;
+    if(!($this->logger InstanceOf Log\LoggerInterface)){
+      $this->logger = new Log\NullLogger;
+    }
   }
 
   // standard accessor magic methods
@@ -38,7 +45,7 @@ class CleverObject implements ArrayAccess
       return $this->_values[$k];
     } else {
       $class = get_class($this);
-      error_log("Clever: Undefined property of $class instance: $k");
+      $this->logger->notice("Clever: Undefined property of $class instance: $k");
       return null;
     }
   }
