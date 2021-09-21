9900e2a5d4121358c401418867f61a2000015117
peterjake <peter@support.webartdesign.com.au>
2021-09-21 17:19:21 +0800

    IQ-3312 - feat: update api version from v2.0 to v2.1

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -80,7 +80,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = 'https://api.clever.com/v2.0';
+    protected $host = 'https://api.clever.com/v2.1';
 
     /**
      * User agent of the HTTP request, set to "PHP-Swagger" by default
